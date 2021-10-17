a53518612d7fda923ee84b567c52f88b95ecd667
Wiktor Chabowski <wiktorchabowski02@gmail.com>
2021-10-17 18:23:31 +0200

    Header avec classe

---
+++ Copy.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef Copy
+
+class Copy {
+
+public:
+
+	Copy(int, int, int);
+	//Copy(const Copy&); //deuxieme constructeur
+	~Copy();
+	int x, y;
+	int* pointer;
+
+};
+
+
+
+#endif
